2a9acf0726d3003b831152b0e7d0eb3273e78b9f
Hugo Fonseca <hugofonseca93@hotmail.com>
2018-03-21 17:26:40 +0000

    Fixed duplicated contents from component_groups.go

--- components.go
+++ components.go
@@ -5,60 +5,77 @@ import (
 )
 
 const (
-	// ComponentGroupVisibilityPublic means "Viewable by public"
-	ComponentGroupVisibilityPublic = 1
-	// ComponentGroupVisibilityLoggedIn means "Only visible to logged in users"
-	ComponentGroupVisibilityLoggedIn = 0
+	// Docs: https://docs.cachethq.io/docs/component-statuses
+
+	// ComponentStatusUnknown means "The component's status is not known."
+	ComponentStatusUnknown = 0
+	// ComponentStatusOperational means "The component is working."
+	ComponentStatusOperational = 1
+	// ComponentStatusPerformanceIssues means "The component is experiencing some slowness."
+	ComponentStatusPerformanceIssues = 2
+	// ComponentStatusPartialOutage means "The component may not be working for everybody."
+	ComponentStatusPartialOutage = 3
+	// ComponentStatusMajorOutage means "The component is not working for anybody."
+	ComponentStatusMajorOutage = 4
 )
 
-// ComponentGroupsService contains REST endpoints that belongs to cachet components.
-type ComponentGroupsService struct {
+// ComponentsService contains REST endpoints that belongs to cachet components.
+type ComponentsService struct {
 	client *Client
 }
 
-// ComponentGroup entity reflects one single component group
-type ComponentGroup struct {
-	ID                      int         `json:"id,omitempty"`
-	Name                    string      `json:"name,omitempty"`
-	Order                   int         `json:"order,omitempty"`
-	Collapsed               int         `json:"collapsed,omitempty"`
-	Visible                 int         `json:"visible,omitempty"`
-	CreatedAt               string      `json:"created_at,omitempty"`
-	UpdatedAt               string      `json:"updated_at,omitempty"`
-	EnabledComponents       []Component `json:"enabled_components,omitempty"`
-	EnabledComponentsLowest []Component `json:"enabled_components_lowest,omitempty"`
-	LowestHumanStatus       string      `json:"lowest_human_status,omitempty"`
+// Tag ...
+type Tag struct {
+	Tag string `json:"tag,omitempty"`
 }
 
-// ComponentGroupResponse reflects the response of /components/groups call
-type ComponentGroupResponse struct {
-	Meta            Meta             `json:"meta,omitempty"`
-	ComponentGroups []ComponentGroup `json:"data,omitempty"`
+// Component entity reflects one single component
+type Component struct {
+	ID          int    `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Link        string `json:"link,omitempty"`
+	Status      int    `json:"status,omitempty"`
+	Order       int    `json:"order,omitempty"`
+	Enabled     bool   `json:"enabled,omitempty"`
+	GroupID     int    `json:"group_id,omitempty"`
+	CreatedAt   string `json:"created_at,omitempty"`
+	UpdatedAt   string `json:"updated_at,omitempty"`
+	DeletedAt   string `json:"deleted_at,omitempty"`
+	StatusName  string `json:"status_name,omitempty"`
+	Tags        []Tag  `json:"tags,omitempty"`
 }
 
-// ComponentGroupsQueryParams contains fields to filter returned results
-type ComponentGroupsQueryParams struct {
-	ID        int    `url:"id,omitempty"`
-	Name      string `url:"name,omitempty"`
-	Order     int    `url:"order,omitempty"`
-	Collapsed bool   `url:"collapsed,omitempty"`
-	Visible   int    `url:"visible,omitempty"`
+// ComponentResponse reflects the response of /components call
+type ComponentResponse struct {
+	Meta       Meta        `json:"meta,omitempty"`
+	Components []Component `json:"data,omitempty"`
+}
+
+// ComponentsQueryParams contains fields to filter returned results
+type ComponentsQueryParams struct {
+	ID      int    `url:"id,omitempty"`
+	Name    string `url:"name,omitempty"`
+	Status  int    `url:"status,omitempty"`
+	Order   int    `url:"order,omitempty"`
+	Enabled bool   `url:"enabled,omitempty"`
+	GroupID int    `url:"group_id,omitempty"`
 	QueryOptions
 }
 
-// componentGroupAPIResponse is an internal type to hide
+// componentApiResponse is an internal type to hide
 // some the "data" nested level from the API.
-// Some calls (e.g. Get or Create) return the component group in the "data" key.
-type componentGroupAPIResponse struct {
-	Data *ComponentGroup `json:"data"`
+// Some calls (e.g. Get or Create) return the component in the "data" key.
+type componentAPIResponse struct {
+	Data *Component `json:"data"`
 }
 
-// GetAll return all component groups that have been created.
+// GetAll return all components that have been created.
 //
-// Docs: https://docs.cachethq.io/reference#get-componentgroups
-func (s *ComponentGroupsService) GetAll(filter *ComponentGroupsQueryParams) (*ComponentGroupResponse, *Response, error) {
-	u := "api/v1/components/groups"
-	v := new(ComponentGroupResponse)
+// Docs: https://docs.cachethq.io/reference#get-components
+func (s *ComponentsService) GetAll(filter *ComponentsQueryParams) (*ComponentResponse, *Response, error) {
+	u := "api/v1/components"
+	v := new(ComponentResponse)
 
 	u, err := addOptions(u, filter)
 	if err != nil {
@@ -69,44 +86,44 @@ func (s *ComponentGroupsService) GetAll(filter *ComponentGroupsQueryParams) (*Co
 	return v, resp, err
 }
 
-// Get return a single component group.
+// Get return a single component.
 //
-// Docs: https://docs.cachethq.io/reference#get-a-component-group
-func (s *ComponentGroupsService) Get(id int) (*ComponentGroup, *Response, error) {
-	u := fmt.Sprintf("api/v1/components/groups/%d", id)
-	v := new(componentGroupAPIResponse)
+// Docs: https://docs.cachethq.io/reference#get-a-component
+func (s *ComponentsService) Get(id int) (*Component, *Response, error) {
+	u := fmt.Sprintf("api/v1/components/%d", id)
+	v := new(componentAPIResponse)
 
 	resp, err := s.client.Call("GET", u, nil, v)
 	return v.Data, resp, err
 }
 
-// Create creates a new component group.
+// Create a new component.
 //
-// Docs: https://docs.cachethq.io/reference#post-componentgroups
-func (s *ComponentGroupsService) Create(c *ComponentGroup) (*ComponentGroup, *Response, error) {
-	u := "api/v1/components/groups"
-	v := new(componentGroupAPIResponse)
+// Docs: https://docs.cachethq.io/reference#components
+func (s *ComponentsService) Create(c *Component) (*Component, *Response, error) {
+	u := "api/v1/components"
+	v := new(componentAPIResponse)
 
 	resp, err := s.client.Call("POST", u, c, v)
 	return v.Data, resp, err
 }
 
-// Update updates a component group.
+// Update updates a component.
 //
-// Docs: https://docs.cachethq.io/reference#put-component-group
-func (s *ComponentGroupsService) Update(id int, c *ComponentGroup) (*ComponentGroup, *Response, error) {
-	u := fmt.Sprintf("api/v1/components/groups/%d", id)
-	v := new(componentGroupAPIResponse)
+// Docs: https://docs.cachethq.io/reference#update-a-component
+func (s *ComponentsService) Update(id int, c *Component) (*Component, *Response, error) {
+	u := fmt.Sprintf("api/v1/components/%d", id)
+	v := new(componentAPIResponse)
 
 	resp, err := s.client.Call("PUT", u, c, v)
 	return v.Data, resp, err
 }
 
-// Delete deletes a component group.
+// Delete deletes a component.
 //
-// Docs: https://docs.cachethq.io/reference#delete-component-group
-func (s *ComponentGroupsService) Delete(id int) (*Response, error) {
-	u := fmt.Sprintf("api/v1/components/groups/%d", id)
+// Docs: https://docs.cachethq.io/reference#delete-a-component
+func (s *ComponentsService) Delete(id int) (*Response, error) {
+	u := fmt.Sprintf("api/v1/components/%d", id)
 
 	resp, err := s.client.Call("DELETE", u, nil, nil)
 	return resp, err
